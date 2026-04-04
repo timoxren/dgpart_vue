0e6d2c6b37cb0659bac83e74acc1904cddba3016
timoxren <lynxrta@gmail.com>
2026-04-04 05:22:39 +0300

    Fix OG 2

--- layouts/pages.vue
+++ layouts/pages.vue
@@ -26,7 +26,7 @@ useHead({
 })
 </script>
 
-<script>
+<script lang="ts">
 import "assets/css/bootstrap.min.css"
 import "assets/css/font-awesome.min.css"
 import "assets/css/linecons-font-style.css"
